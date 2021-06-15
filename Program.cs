f5b4d44ee9b566d24382e26b9bb33d29fc21bbc1
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-06-15 15:06:31 +0200

    testar att fimpa main. Verkar fungera numera

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
 
-namespace Ovn1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Hello World!");
+  
