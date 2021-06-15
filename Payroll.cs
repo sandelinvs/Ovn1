ee359a7c1ba7f602fe9ffed574732a124d6d5c31
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-06-15 15:13:20 +0200

    lite klasser

---
+++ Payroll.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovn1
+{
+    public class Employee
+    {
+        public string Name { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        public Employee(string name, decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+
+    public interface IPayroll : IEnumerable<Employee>
+    {
+        void AddEmployee(string name, decimal salary);
+    }
+
+    public class Payroll : IPayroll
+    {
+        public void AddEmployee(string name, decimal salary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
